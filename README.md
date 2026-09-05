99be8f88857abb65029df4ef8a356bec7211a880
pemsbems <egor2017s@mail.ru>
2026-09-05 16:44:29 +0000

    add brain-progression and asciinema

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 [![hexlet-check](https://github.com/pemsbems/devops-engineer-from-scratch-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/pemsbems/devops-engineer-from-scratch-project-49/actions/workflows/hexlet-check.yml)
 
-Asciinema brain-even:
+Asciinema:
 https://asciinema.org/a/QH6GYZJQTgsg9U7t
 
 Asciinema brain-gcd:
 https://asciinema.org/a/B5xizdCN9n6ThQJ8
 
+Asciinema brain-progression:
+https://asciinema.org/a/zOFqmAc5LfO0LarT
---
+++ brain_games/games/progression.py
@@ -0,0 +1,30 @@
+import random
+from brain_games.main_engine import game_engine
+
+
+def generate(start, step, length):
+    progression = []
+    for i in range(length):
+        currentElement = start + i * step
+        progression.append(currentElement)
+    return progression
+
+
+def task():
+    length = random.randint(5, 10)  
+    start = random.randint(1, 20)   
+    step = random.randint(1, 10)    
+    
+    progression = generate(start, step, length)
+    unknow_numb = random.randint(0, length - 1)
+    unknow = progression[unknow_numb]
+    
+    progression[unknow_numb] = ".."
+    
+    question = " ".join(map(str, progression))
+    return question, str(unknow)
+
+
+def run_game():
+    rules = "What number is missing in the progression?"
+    game_engine(task, rules)
